9c1861b10fb04944a336a89cb134f3db693d92f1
Nicobecs <38060079+Nicobecs@users.noreply.github.com>
2024-07-30 23:01:03 -0400

    Se agrega jquery con efecto smooth scroll

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('nav a').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 200, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
